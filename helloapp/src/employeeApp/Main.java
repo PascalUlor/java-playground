caf9a284a58096d21186a515422ab31f78054f7d
PascalUlor <pascalulor@yahoo.com>
2020-09-11 11:27:52 +0100

    hello deploy

---
+++ helloapp/src/employeeApp/Main.java
@@ -0,0 +1,16 @@
+package employeeApp;
+
+
+/*
+Employee
+    Companies
+    Health Plan
+    */
+public class Main {
+    private static void workWithData(){
+        System.out.println("Welcome to Java");
+    }
+    public static void main(String[] args){
+        workWithData();
+    }
+}
